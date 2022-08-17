053cc2c60b945b971b4935978945f7612c4b3f98
elenaburdin <66010914+elenaburdin@users.noreply.github.com>
2022-08-17 21:43:21 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,19 @@
+package academy.pragramlearning;
+
+public class Main {
+    public static void main(String[] args) {
+        int myVariable = 50;
+        myVariable++;
+        myVariable--;
+        System.out.println("This is a statement");
+
+        System.out.println("This is" +
+                " another" +
+                " still more.");
+
+        int anotherVariable = 50;
+        System.out.println("This is another one");
+
+
+    }
+}
